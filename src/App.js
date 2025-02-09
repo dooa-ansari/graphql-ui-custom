bfa152987e31c1fd1a115d65ab609d20ee1b7180
dooa-ansari <94571027+dooamindshine@users.noreply.github.com>
2025-02-09 14:42:33 +0100

    data matched to similar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -11,23 +12,67 @@ import Dropdown from "react-bootstrap/Dropdown";
 import DropdownButton from "react-bootstrap/DropdownButton";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
 
+const Query =
+  "query { head { vars link } results { distinct ordered bindings { distribution{value datatype type xmlLang} title{value datatype type xmlLang} mediaType{value datatype type xmlLang} modified{value datatype type xmlLang} identifier{value datatype type xmlLang} accessURL{value datatype type xmlLang} description{value datatype type xmlLang} geometry{value datatype type xmlLang} license{value datatype type xmlLang} publisherName{value datatype type xmlLang} maintainerEmail{value datatype type xmlLang} } } }";
+
 function App() {
+  const [selectedOption, setSelectedOption] = useState("");
+  const [query, setQuery] = useState(Query);
+  const [queryResult, setQueryResult] = useState("")
+
+  const runGraphQlQuery = async () => {
+    const response = await fetch("http://127.0.0.1:8000/graphql/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({ query }),
+    });
+    const result = await response.json();
+    const formattedData = JSON.stringify(result, null, 2).replace(/\\/g, "");
+    setQueryResult(formattedData)
+    console.log(result);
+  };
+
+  const handleSelect = async (eventKey) => {
+    setSelectedOption(eventKey);
+
+    try {
+      const response = await fetch(
+        `http://127.0.0.1:8000/machine_data/machine_data?dataset=${eventKey}`
+      );
+      const data = await response.json();
+      console.log("API Response:", data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
   return (
     <Container>
       <Row>
         <Col>
-          <div>
+          <div className="p-1">
             <h2>Sparql-to-Graphql Research Project</h2>
           </div>
         </Col>
         <Col>
-          <Stack direction="horizontal" gap={3}>
-            <Nav defaultActiveKey="/home" as="ul">
+          <Stack
+            direction="horizontal"
+            gap={3}
+            className="p-1 d-flex justify-content-end"
+          >
+            <Nav
+              className="p-1 d-flex justify-content-end"
+              defaultActiveKey="/home"
+              as="ul"
+            >
               <Nav.Item as="li">
-                <Nav.Link href="/home">How To</Nav.Link>
+                <Nav.Link href="/howto">How To</Nav.Link>
               </Nav.Item>
               <Nav.Item as="li">
-                <Nav.Link eventKey="link-1">About</Nav.Link>
+                <Nav.Link href="/about">About</Nav.Link>
               </Nav.Item>
             </Nav>
           </Stack>
@@ -36,10 +81,20 @@ function App() {
       <Row>
         <Col>
           <Stack direction="horizontal" gap={3}>
-            <DropdownButton id="dropdown-basic-button" title="Dropdown button">
-              <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-              <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-              <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+            <DropdownButton
+              className="mt-3 p-1"
+              id="datasets"
+              title="Datasets Selections"
+              onSelect={handleSelect}
+            >
+              <Dropdown.Item eventKey="Animal Parks">
+                Animal Parks
+              </Dropdown.Item>
+              <Dropdown.Item eventKey="Parking ticket machines">
+                Parking ticket machines
+              </Dropdown.Item>
+              <Dropdown.Item eventKey="Schools">Schools</Dropdown.Item>
+              {selectedOption && <p>Selected: {selectedOption}</p>}
             </DropdownButton>
           </Stack>
         </Col>
@@ -47,44 +102,52 @@ function App() {
       <Row>
         <Col>
           <Stack>
-            <div style={{ backgroundColor: "#ffb3ba" }}>
+            <div className="mt-5 p-3" style={{ backgroundColor: "#ffb3ba" }}>
               <Row>
-                <Col>Graphql Query</Col>
                 <Col>
-                  <ButtonGroup aria-label="Basic example">
-                    <Button variant="secondary">Copy</Button>
-                    <Button variant="secondary">Edit</Button>
+                  <h3>Graphql Query</h3>
+                </Col>
+                <Col>
+                  <ButtonGroup className="mb-3 d-flex justify-content-end">
+                    <Button variant="primary">Copy</Button>
+                    <Button variant="success">Edit</Button>
                   </ButtonGroup>
                 </Col>
               </Row>
-              <Form.Group
-                className="mb-3"
-                controlId="exampleForm.ControlTextarea1"
-              >
-                <Form.Control as="textarea" rows={3} />
+              <Form.Group className="mb-3" controlId="graphqlquery">
+                <Form.Control
+                  as="textarea"
+                  rows={15}
+                  defaultValue={query}
+                />
               </Form.Group>
             </div>
 
-            <div>
-              <Button variant="success">Run</Button>
+            <div className="mt-5 d-flex justify-content-end">
+              <Button
+                onClick={runGraphQlQuery}
+                className="px-5"
+                variant="primary"
+              >
+                Run
+              </Button>
             </div>
           </Stack>
         </Col>
         <Col>
-          <div style={{ backgroundColor: "#ffb3ba" }}>
+          <div className="mt-5 p-3" style={{ backgroundColor: "#ffb3ba" }}>
             <Row>
-              <Col>Results</Col>
               <Col>
-                <ButtonGroup aria-label="Basic example">
-                  <Button variant="secondary">Copy</Button>
+                <h3>Results</h3>
+              </Col>
+              <Col>
+                <ButtonGroup className="mb-3 d-flex justify-content-end">
+                  <Button variant="primary">Copy</Button>
                 </ButtonGroup>
               </Col>
             </Row>
-            <Form.Group
-              className="mb-3"
-              controlId="exampleForm.ControlTextarea1"
-            >
-              <Form.Control as="textarea" rows={3} />
+            <Form.Group className="mb-3" controlId="resultsquery">
+              <Form.Control defaultValue={queryResult} as="textarea" rows={15} />
             </Form.Group>
           </div>
         </Col>
